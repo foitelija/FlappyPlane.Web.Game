7862601c7ebc886d908b6d935c1a8354e4a2bee4
Foitelija <foitelija777@gmail.com>
2022-08-01 12:55:17 +0300

    feat: Gravity to Fall & StartGame + GameOver & Restart game after fall

--- Models/BirdModel.cs
+++ Models/BirdModel.cs
@@ -3,5 +3,10 @@
     public class BirdModel
     {
         public int DistanceFromGround { get; set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
     }
 }
