228d07146b3e0f9994853ea50e1a3121e3dc7309
Foitelija <foitelija777@gmail.com>
2022-08-01 12:34:11 +0300

    feat: Models, Style

---
+++ Models/BirdModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FlappyPlane.Web.Game.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+    }
+}
---
+++ Models/GameManager.cs
@@ -0,0 +1,12 @@
+﻿namespace FlappyPlane.Web.Game.Models
+{
+    public class GameManager
+    {
+        public BirdModel Bird { get; set; }
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+    }
+}
